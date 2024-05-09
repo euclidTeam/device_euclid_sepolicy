3fd4ac9152650a95ada6a4da17a9d4c67e36258d
Mrick343 <mrinmoy8429de@gmail.com>
2024-05-09 15:30:33 +0530

    sepolicy: Adapt to euclidOS

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,8 +5,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/euclid/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/euclid/sepolicy/atv/private
