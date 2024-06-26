4b0485b64f74c0210f1b3829c21a3506d28ea2c6
Mrick343 <mrinmoy8429de@gmail.com>
2024-06-26 19:33:08 +0530

    sepolicy: Adapt to euclidOS
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from euclid
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/euclid/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/euclid/sepolicy/atv/private
